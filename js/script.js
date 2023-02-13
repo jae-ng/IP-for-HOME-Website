6629c63050805ae82136fe4b3ec64e3835367392
jae-ng <s10242508@connect.np.edu.sg>
2023-02-14 02:25:59 +0800

    setting up image slider and banner images

--- js/script.js
+++ js/script.js
@@ -35,3 +35,20 @@ closer.onclick = () =>{
     cart.classList.remove('active');
     loginForm.classList.remove('active');
 }
+
+//image slider
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
