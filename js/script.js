159c53926c2a5f5c7ef40c53b7c51cadf180e583
jae-ng <s10242508@connect.np.edu.sg>
2023-02-14 00:10:05 +0800

    coded navbar features, hamburger, cart, login form

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//open hamburger
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-button').onclick = () =>{
+    closer.style.display = 'block';
+    navbar.classList.toggle('active');
+}
+
+//open cart
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-button').onclick = () =>{
+    closer.style.display = 'block';
+    cart.classList.toggle('active');
+}
+
+//open login
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-button').onclick = () =>{
+    closer.style.display = 'block';
+    loginForm.classList.toggle('active');
+}
+
+//close hamburger/cart/login
+
+let closer = document.querySelector('#closer');
+
+closer.onclick = () =>{
+    closer.style.display = 'none';
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
